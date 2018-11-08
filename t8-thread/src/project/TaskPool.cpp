1ab12e88793ffdc0bdac26870fed39cb9c6439d5
Nikita Korobkov <nikkorobk@gmail.com>
2018-11-08 15:30:26 +0300

    t8-thread: done

--- t8-thread/src/project/TaskPool.cpp
+++ t8-thread/src/project/TaskPool.cpp
@@ -8,7 +8,48 @@
 
 #include <queue>
 
-// определяем задачу
+
+template<typename T>
+void merge(T xf, T xl, T yf, T yl) {
+    T f = xf;
+
+    unsigned long const size = yl - xf;
+    std::vector<typename T::value_type> buf;
+    int i = 0;
+    while (i < size) {
+        if (xf == xl) {
+            buf.insert(buf.begin() + i, yf, yl);
+            break;
+        }
+        if (yf == yl) {
+            buf.insert(buf.begin() + i, xf, xl);
+            break;
+        }
+        if (*xf >= *yf) {
+            buf.push_back(*xf);
+            xf++;
+        } else {
+            buf.push_back(*yf);
+            yf++;
+        }
+        i++;
+    }
+    std::copy(buf.begin(), buf.end(), f);
+
+}
+
+template<typename T>
+void m_sort(T first, T last) {
+    long size = std::distance(first, last);
+    if (size < 2)
+        return;
+    T middle = std::next(first, size / 2);
+    m_sort(first, middle);
+    m_sort(middle, last);
+    merge(first, middle, middle, last);
+}
+
+
 template<class T>
 struct SortTask {
     T first;
@@ -24,13 +65,15 @@ struct SortTask {
 template<class T>
 class SortingMachine {
 public:
+    unsigned int NK = std::thread::hardware_concurrency();
     std::mutex mtx;
     std::condition_variable cv; // условная переменная
     std::queue<SortTask<T> *> tasks;
     T first;
     T last;
     std::atomic<bool> stopped{false}; // состояние
-
+    std::vector<std::thread> threads{NK}; // потоки
+    int busy_t = 0;
 
     SortingMachine(T first, T last) : first(first), last(last) {
         tasks = std::queue<SortTask<T> *>();
@@ -41,23 +84,25 @@ public:
         std::lock_guard<std::mutex> lock(mtx);
         tasks.push(t);
 
-        std::cout << " notifying!! tasks size = " << std::to_string(tasks.size()) << "\n";
+        //std::cout << " notifying!! tasks size = " << std::to_string(tasks.size()) << "\n";
 
         cv.notify_one(); // оповестить один из ждущих потоков
     }
 
     void loop() {
         std::unique_lock<std::mutex> lock{mtx};
-        std::cout << "thread loop??\n";
+        //std::cout << "thread loop??\n";
         cv.wait(lock, [this, &lock]() {
-            std::cout << "thread working, tasks size = " << std::to_string(tasks.size()) << "\n";
+            //std::cout << "thread working, tasks size = " << std::to_string(tasks.size()) << "\n";
             while (!tasks.empty()) {
-                std::cout << "task detected\n";
+                //std::cout << "task detected\n";
                 SortTask<T> *task = tasks.front();
                 tasks.pop();
+                busy_t +=1;
                 lock.unlock(); // lock_guard наоборот
                 do_sort(task);
                 lock.lock();
+                busy_t -=1;
             }
             bool d = stopped;
             return d;
@@ -65,31 +110,31 @@ public:
 
     }
 
-    std::vector<std::thread> threads{8}; // потоки
     void start() {
 
         for (auto &thr : threads) {
-            std::cout << "starting thread##\n";
+            //std::cout << "starting thread##\n";
             thr = std::thread{[this]() { this->loop(); }};
         }
         stopped = false; // изменяем состояние
-        std::cout << "threads started\n";
+        //std::cout << "threads started "<<NK<<"\n";
+
     }
 
     void stop() {
         stopped = true;
         cv.notify_all();
-        std::cout << "stop called\n";
+        //std::cout << "stop called\n";
     }
 
     void wait() { // ожидание завершения потоков
-        std::cout << "waiting with " << std::to_string(this->tasks.size()) << " tasks in queue\n";
+        //std::cout << "waiting with " << std::to_string(this->tasks.size()) << " tasks in queue\n";
         for (auto &thr : threads) {
-            std::cout << "waiting with " << std::to_string(this->tasks.size()) << " tasks in queue\n";
+            //std::cout << "waiting with " << std::to_string(this->tasks.size()) << " tasks in queue\n";
 
             if (thr.joinable()) { thr.join(); }
         }
-        std::cout << "threads stopped\n";
+        //std::cout << "threads stopped\n";
 
     }
 
@@ -97,7 +142,7 @@ public:
         SortTask<T> *t = new SortTask<T>(this->first, this->last);
 
         this->push(t);
-        while (!(t->flag)){}
+        while (!(t->flag)) {}
         this->stop();
         this->wait();
     }
@@ -115,26 +160,46 @@ public:
 
         //std::cout << "pushing from sorting task\n";
         //std::cout << std::this_thread::get_id() << std::endl;
+        //std::cout << "choosing option " << busy_t <<"\n";
 
-        this->push(t1);
-        this->push(t2);
+        if (there_is_a_thread() && size>1000) {
+            this->push(t1);
+            this->push(t2);
 
-        //std::cout << "waiting in sorting task " << std::to_string(this->tasks.size()) << " tasks in queue\n";
-        //std::cout << std::this_thread::get_id() << " mtx: " << std::endl;
+            //std::cout << "waiting in sorting task " << std::to_string(this->tasks.size()) << " tasks in queue\n";
+            //std::cout << std::this_thread::get_id() << " mtx: " << std::endl;
 
-        //cv.notify_all();
-        while (!(t1->flag));
-        while (!(t2->flag));
-        std::cout << "merge in sorting task\n";
-        std::cout << std::this_thread::get_id() << std::endl;
+            //cv.notify_all();
+            while (!(t1->flag));
+            while (!(t2->flag));
+            //std::cout << "merge in sorting task\n";
+            //std::cout << std::this_thread::get_id() << std::endl;
+        } else {
+            //do_sort(t1);
+            //do_sort(t2);
+
+            m_sort(middle, task->last);
+            m_sort(task->first,middle);
+
+        }
 
 
         merge_a(task->first, middle, middle, task->last);
-        std::cout << "releasing sorting task\n";
-        std::cout << "waiting with "<<std::to_string(this->tasks.size())<<" tasks in queue\n";
+        //std::cout << "releasing sorting task\n";
+        //std::cout << "waiting with " << std::to_string(this->tasks.size()) << " tasks in queue\n";
 
         task->flag = true;
-        std::cout<<"flag set on task";
+        //std::cout << "flag set on task";
+    }
+
+    bool there_is_a_thread() {
+        // this is wrong since loop is rolling so nothing is joinable
+        if (busy_t < (NK-1)) {
+            //std::cout << "free thread!\n";
+            return true;
+        }
+        //std::cout << "inplace\n";
+        return false;
     }
 
 
--- t8-thread/src/project/main.cpp
+++ t8-thread/src/project/main.cpp
@@ -6,99 +6,68 @@
 #include <future>
 #include <thread>
 #include "TaskPool.cpp"
+#include <random>
 
+std::random_device rd;  //Will be used to obtain a seed for the random number engine
+std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+std::uniform_int_distribution<> dis(1, 10000);
 
-
-template<typename T>
-void merge(T xf, T xl, T yf, T yl) {
-    T f = xf;
-
-    unsigned long const size = yl - xf;
-    std::vector<typename T::value_type> buf;
-    int i = 0;
-    while (i < size) {
-        if (xf == xl) {
-            buf.insert(buf.begin() + i, yf, yl);
-            break;
-        }
-        if (yf == yl) {
-            buf.insert(buf.begin() + i, xf, xl);
-            break;
-        }
-        if (*xf >= *yf) {
-            buf.push_back(*xf);
-            xf++;
-        } else {
-            buf.push_back(*yf);
-            yf++;
-        }
-        i++;
-    }
-    std::copy(buf.begin(), buf.end(), f);
-
-}
-
-template<typename T>
-void m_sort(T first, T last) {
-    long size = std::distance(first, last);
-    if (size < 2)
-        return;
-    T middle = std::next(first, size / 2);
-    m_sort(first, middle);
-    m_sort(middle, last);
-    merge(first, middle, middle, last);
-}
-
-
-
-
-int main() {
+void test_on_size(int n){
     std::vector<int> acc1;
     std::vector<int> acc2;
     std::vector<int> acc3;
-
     std::chrono::milliseconds sec(1);
-
-    for (int i = 1; i < 10; i++) {
-        acc1.push_back(i);
-        acc2.push_back(i);
-        acc3.push_back(i);
+    int v;
+    for (int i = 1; i < n; i++) {
+        v = dis(gen);
+        acc1.push_back(v);
+        acc2.push_back(v);
+        acc3.push_back(v);
     }
     auto SM = new SortingMachine<std::vector<int>::iterator>(acc2.begin(), acc2.end());
-
     std::chrono::system_clock::time_point t = std::chrono::system_clock::now();
     SM->start();
     SM->sort();
-
-    //SM->stop();
-    //SM->wait();
-
     auto async = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - t).count();
-
-
     t = std::chrono::system_clock::now();
-
-
     m_sort(acc1.begin(), acc1.end());
-
     auto reg = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - t).count();
-
-    std::cout << "reg = " << reg << " async = " << async<<"\n";
     int i=0;
-//    for(;i<acc1.size();i++){
-//        if(acc1[i] != acc2[i]){
-//            std::cout<<"WA"<<std::to_string(i);
-//            break;
-//        }
-//    }
-
-    for(;i<10;i++){
-
-        std::cout<<std::to_string(acc1[i]) <<"   "<<std::to_string(acc2[i])<<"\n";
-
+    for(;i<acc1.size();i++){
+        if(acc1[i] != acc2[i]){
+            std::cout<<"WA"<<std::to_string(i);
+            break;
+        }
     }
-    if(i == acc1.size()){
-        std::cout<<"Sorting correct";
+    bool correct  = i == acc1.size();
+    std::cout<< "size=";
+    std::cout.width(9);
+    std::cout<< n ;
+    std::cout<< " reg = ";
+    std::cout.width(10);
+    std::cout<< reg;
+    std::cout<< " async = ";
+    std::cout.width(10);
+    std::cout<< async;
+    std::cout<<" Correctness = ";
+    std::cout<< std::to_string(correct) <<"\n";
+}
+
+int main() {
+    std::cout.fill(' ');
+    std::vector<int> sizes = {10, 100, 1000, 10000, 100000, 500000};
+    for (int s : sizes){
+        test_on_size(s);
     }
     return 0;
 }
+
+/*
+ *
+size=       10 reg =         49 async =       3636 Correctness = 1
+size=      100 reg =        159 async =        417 Correctness = 1
+size=     1000 reg =       2422 async =       2830 Correctness = 1
+size=    10000 reg =      24931 async =      21269 Correctness = 1
+size=   100000 reg =     448361 async =     328648 Correctness = 1
+size=   500000 reg =    2437480 async =    1898943 Correctness = 1
+ */
